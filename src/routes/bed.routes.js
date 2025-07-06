d526aae018e44dc448ee9e366b246384cef09ffd
Ashi-Verma1758 <ashiverma1758@gmail.com>
2025-07-06 07:37:20 +0530

    changes

--- src/routes/bed.routes.js
+++ src/routes/bed.routes.js
@@ -3,16 +3,14 @@ import {
   addBed,
   updateBed,
   getBedsByHospital,
-  getBedById,
 } from '../controllers/bed.controller.js';
 import { requireAuth } from '../middlewares/auth.middleware.js';
 import { authorizeRoles } from '../middlewares/role.middleware.js';
 
 const router = express.Router();
-router.get('/bed/:id', requireAuth, getBedById);
+router.get('/:hospitalId', getBedsByHospital);
 
 router.post('/', requireAuth, authorizeRoles('admin', 'hospital-staff'), addBed);
 router.put('/:id', requireAuth, authorizeRoles('admin', 'hospital-staff'), updateBed);
-router.get('/:hospitalId', getBedsByHospital);
 
 export default router;
